4818a3e0eebb7269fdfa2c50a1f1277fd02b2af2
alexandra <alexandraschurman@gmail.com>
2022-02-06 18:59:25 -0800

    half fetch request done

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import Reac,{ useEffect } from "react";
 import PokemonPage from "./PokemonPage";
 
 function App() {
+
+  const pokemon_url_1 = 'http://localhost:3000'
+  const pokemon_url = 'http://localhost:3000'
+
+  useEffect(()=> 
+  fetch((pokemon_url)
+  .then((r)=> r.json())
+  .then((data)=> {
+    
+  })
+  )
+
+
   return (
     <div className="App">
       <PokemonPage />
--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,6 +3,9 @@ import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
 function PokemonCollection() {
+
+  const cardsToRender = 
+
   return (
     <Card.Group itemsPerRow={6}>
       <h1>Hello From Pokemon Collection</h1>
